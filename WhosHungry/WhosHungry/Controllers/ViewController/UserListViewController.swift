741d064d010a61526546f061c33c692b0b41d2f1
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-04-17 14:33:41 -0600

    add join and host buttons/bunctions to userListVC

--- WhosHungry/WhosHungry/Controllers/ViewController/UserListViewController.swift
+++ WhosHungry/WhosHungry/Controllers/ViewController/UserListViewController.swift
@@ -22,7 +22,16 @@ class UserListViewController: UIViewController, MCSessionDelegate, MCBrowserView
     }
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        
+        switch state {
+        case .connected:
+            print("Connected to \(peerID.displayName)")
+        case .connecting:
+            print("Connecting: \(peerID.displayName)")
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        default:
+            print("")
+        }
     }
     
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
@@ -42,12 +51,30 @@ class UserListViewController: UIViewController, MCSessionDelegate, MCBrowserView
     }
     
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        
+        browserViewController.dismiss(animated: true, completion: nil)
     }
     
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        
+        browserViewController.dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func hostButtonTapped(_ sender: Any) {
+        hostSession()
+    }
+    
+    @IBAction func joinButtonTapped(_ sender: Any) {
+        joinSession()
+    }
+    
+    func hostSession() {
+        let advertiser = MCAdvertiserAssistant(serviceType: "mg-testing", discoveryInfo: nil, session: session!)
+        advertiser.start()
+    }
+    
+    func joinSession() {
+        let browser = MCBrowserViewController(serviceType: "mg-testing", session: session!)
+        browser.delegate = self
+        self.present(browser, animated: true, completion: nil)
+    }
+    
 }//End of class
