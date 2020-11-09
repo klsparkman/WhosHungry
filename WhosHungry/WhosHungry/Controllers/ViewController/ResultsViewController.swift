2bff92391d0a2eccd13f35c0e4525a0c611db523
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-11-09 15:19:40 -0700

    add compareSubmittedVotes func to results VC. pulling info correctly

--- WhosHungry/WhosHungry/Controllers/ViewController/ResultsViewController.swift
+++ WhosHungry/WhosHungry/Controllers/ViewController/ResultsViewController.swift
@@ -12,29 +12,50 @@ class ResultsViewController: UIViewController {
     
     static let shared = ResultsViewController()
     var voteDict: [String : Int] = [:]
-    var gameUID: String?
-
+    var snapshotListenerData: [String : Any] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        Firebase.shared.listenForSubmittedVotes(gameUID: <#T##String#>)
-        findHighestVotedRestaurant()
+        comparingSubmittedVotes()
     }
     
-    private func findHighestVotedRestaurant() {
-        for totals in self.voteDict.values {
-            if totals != 0 {
-                if totals == RestaurantController.shared.users.count {
-                    print("TOTALS: \(voteDict.keys) : \(totals)")
+    //    private func findHighestVotedRestaurant() {
+    //        for totals in self.voteDict.values {
+    //            if totals != 0 {
+    //                if totals == RestaurantController.shared.users.count {
+    //                    print("TOTALS: \(voteDict.keys) : \(totals)")
+    //                }
+    //            } else {
+    //                if totals == 0 {
+    //                    noRestaurantVote()
+    //                }
+    //            }
+    //        }
+    //    }
+    
+    func comparingSubmittedVotes() {
+        guard let gameUID = Firebase.shared.currentGame?.uid else {return}
+        guard let game = Firebase.shared.currentGame else {return}
+        
+        Firebase.shared.listenForSubmittedVotes(gameUID: gameUID) { (result) in
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(_):
+                Firebase.shared.fetchGame(withinviteCode: game.inviteCode) { (result) in
+                    switch result {
+                    case .success(let game):
+                        if let game = game {
+                            print("Here are the submitted votes: \(game.submittedVotes)")
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
                 }
-            } else {
-                if totals == 0 {
-                    noRestaurantVote()
-                }
+//                print("Here are the submitted votes: \(game.submittedVotes.count)")
             }
         }
     }
-    
-    
     
     private func noRestaurantVote() {
         let alert = UIAlertController(title: "You didn't like any of these options?", message: "You must swipe right on at least 1 restaurant", preferredStyle: .alert)
