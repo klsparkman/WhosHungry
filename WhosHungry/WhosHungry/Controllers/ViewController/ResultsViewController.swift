2fa876cd3fc2ccc7b751f91b2da4215946d5c015
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-12-02 16:13:18 -0700

    use listenForUsers func to track players.count

--- WhosHungry/WhosHungry/Controllers/ViewController/ResultsViewController.swift
+++ WhosHungry/WhosHungry/Controllers/ViewController/ResultsViewController.swift
@@ -13,6 +13,7 @@ class ResultsViewController: UIViewController {
     static let shared = ResultsViewController()
     var likedRestDict: [String : Int] = [:]
     var likes: [String] = []
+    var result: Int?
     
     @IBOutlet weak var restaurantRestultLabel: UILabel!
     
@@ -20,56 +21,44 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Firebase.shared.listenForLikes { (result) in
+            self.likes = []
+            self.result = result.count
+            print("RESULT COUNT: \(result.count)")
+            for vote in result {
+                self.likes.append(vote)
+            }
+            print("LIKES: \(self.likes)")
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.becomeFirstResponder()
-        
-        Firebase.shared.listenForLikes { (result) in
-            self.likes = []
-            for vote in result {
-                self.likes.append(vote)
-                print("VOTES: \(self.likes)")
-            }
+        let players = Firebase.shared.playerCount
+        if players ==  result {
+            self.findMatches()
+        } else {
+            print("Still waiting for everyone to finish swiping")
         }
-        
-//        Firebase.shared.startListener {
-//            guard let game = Firebase.shared.currentGame else {return}
-//            if game.submittedVotes.count + 1 == game.users.count {
-//                //                _ = Firebase.shared.finishedVotes
-//                self.findMatches()
-//            } else {
-//                print("Still waiting for everyone to finish")
-//            }
-//        }
     }
     
     func findMatches() {
         var restaurantVotes: [String : Int] = [:]
-        let voteValues = [Firebase.shared.votes]
+        let voteValues = self.likes
+//        let voteValues = [Firebase.shared.votes]
         
         for personsVotes in voteValues {
-            for restaurant in personsVotes {
-                if restaurantVotes[restaurant] != nil {
-                    restaurantVotes[restaurant]! += 1
+//            for restaurant in personsVotes {
+                if restaurantVotes[personsVotes] != nil {
+                    restaurantVotes[personsVotes]! += 1
                 } else {
-                    restaurantVotes[restaurant] = 1
+                    restaurantVotes[personsVotes] = 1
                 }
-            }
+//            }
         }
         print("VOTE DICTIONARY: \(restaurantVotes)")
     }
 }// End of class
-
-/*
- 
- RESULTSVC VOTE VALUES: [["[\"The Park Café\", \"Pulp Lifestyle Kitchen-Downtown\", \"Bruges Belgian Bistro\", \"The Original Pancake House - Sugarhouse\", \"Hruska\\\'s Kolaches\", \"Even Stevens Sandwiches\"]", "[\"The Park Café\", \"Sweet Lake Biscuits & Limeade\", \"Pig & a Jelly Jar\", \"Bruges Belgian Bistro\", \"Hruska\\\'s Kolaches\"]"]]
- 
- 
- PERSONS VOTES: ["[\"The Park Café\", \"Pulp Lifestyle Kitchen-Downtown\", \"Bruges Belgian Bistro\", \"The Original Pancake House - Sugarhouse\", \"Hruska\\\'s Kolaches\", \"Even Stevens Sandwiches\"]", "[\"The Park Café\", \"Sweet Lake Biscuits & Limeade\", \"Pig & a Jelly Jar\", \"Bruges Belgian Bistro\", \"Hruska\\\'s Kolaches\"]"]
- 
- 
- RESTAURANT: ["The Park Café", "Pulp Lifestyle Kitchen-Downtown", "Bruges Belgian Bistro", "The Original Pancake House - Sugarhouse", "Hruska\'s Kolaches", "Even Stevens Sandwiches"]
- 
- 
- RESTAURANT: ["The Park Café", "Sweet Lake Biscuits & Limeade", "Pig & a Jelly Jar", "Bruges Belgian Bistro", "Hruska\'s Kolaches"]
- */
